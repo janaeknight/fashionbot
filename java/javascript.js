9988ff109f7d45dd763addb95e040656487cb59f
Knight <janae.jpeg@gmail.com>
2019-10-08 13:47:23 -0500

    Initial commit

---
+++ java/javascript.js
@@ -0,0 +1,63 @@
+// Fashion Bot
+
+// Based on the temperature range, weather conditions and activity choose the appropriate outfit for the day. Must use conditionals, nested conditionals and prompts to return outfit in a 'alert' box. hint: MDN alert
+
+// ~~~~~~~~~
+
+var temp = prompt("What is the current temperature (in °F)?");
+var cool = 0;       var coolWear = "";
+var cold = 0;       var coldWear = "";
+var warm = 0;       var warmWear = "";
+var hot = 0;        var hotWear = "";
+var rainy = 0;      var rainyWear = "";
+var dry = 0;        var dryWear = "";
+var humid = 0;      var humidWear = "";
+
+if (temp <=11) {
+    alert("Did you ever consider NOT going outside?");
+}
+if (temp >=12 && temp <= 46) {
+    cold = cold + 1;
+    var coldRain = prompt("Will it snow/hail/rain? (Yes/No)");
+        if (coldRain == "yes") {
+            rainy = rainy + 1;
+        }
+    var coldHumid = prompt("Is it humid? (Yes/No)");
+        if (coldHumid == yes) {
+            humid = humid + 1;
+        }
+}
+if (temp >=47 && temp <=69) {
+    cool = cool + 1;
+    var coolRain = prompt("Will it rain? (Yes/No)");
+    if (coolRain == "yes") {
+        rainy = rainy + 1;
+    }
+    var coolHumid = prompt("Is it humid? (Yes/No)");
+    if (coolHumid == yes) {
+        humid = humid + 1;
+    }
+}
+if (temp >=70 && temp <=81) {
+    var warmRain = prompt("Will it rain? (Yes/No)");
+    if (warmRain == "yes") {
+        rainy = rainy + 1;
+    }
+    var warmHumid = prompt("Is it humid? (Yes/No)");
+    if (warmHumid == yes) {
+        humid = humid + 1;
+    }
+}
+if (temp >=81 && temp <=103) {
+    var hotRain = prompt("Will it rain? (Yes/No)");
+    if (hotRain == "yes") {
+        rainy = rainy + 1;
+    }
+    var hotHumid = prompt("Is it humid? (Yes/No)");
+    if (hotHumid == yes) {
+        humid = humid + 1;
+    }
+}
+if (temp>=104) {
+    alert("WARNING: It's way too hot to be outside comfortably for longer than 20 seconds. If you decide to go outside anyways, I recommended not wearing any clothes at all. Maybe take off your skin too.");
+}
